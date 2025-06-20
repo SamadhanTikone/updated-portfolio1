7de189265fb6325aaae6bc82cf723919a0666e0e
SamadhanTikone <samadhantikone003@gmail.com>
2025-06-20 16:01:14 +0530

    view button added

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -10,6 +10,7 @@ import {
   Star,
   ExternalLink,
   ChevronRight,
+  Eye
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
@@ -162,18 +163,37 @@ const Resume = () => {
                 hands-on experience in building scalable web applications
               </p>
 
-              <motion.div
+              <div className="flex flex-col justify-center gap-4 sm:gap-10 sm:flex-row ">
+                <motion.div
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }}
               >
-                <Button
+              <a href="./Samadhan_Tikone_Resume.pdf">
+                  <Button
+                  size="lg"
+                  className="bg-portfolio-teal hover:bg-portfolio-teal/90 text-white group animate-pulse hover:animate-none"
+                >
+                  <Eye className="w-5 h-5 mr-2 group-hover:animate-bounce-gentle" />
+                  View Resume
+                </Button>
+              </a>
+              </motion.div>
+
+               <motion.div
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+              >
+              <a href="./Samadhan_Tikone_Resume.pdf"  download={true}>
+                  <Button
                   size="lg"
                   className="bg-portfolio-teal hover:bg-portfolio-teal/90 text-white group animate-pulse hover:animate-none"
                 >
                   <Download className="w-5 h-5 mr-2 group-hover:animate-bounce" />
                   Download My Resume (PDF)
                 </Button>
+              </a>
               </motion.div>
+              </div>
             </motion.div>
 
             <div className="grid lg:grid-cols-3 gap-8">
